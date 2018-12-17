20e127389439ceaf34e035a446e67075f5b39009
sowizdrzal <45939141+sowizdrzal@users.noreply.github.com>
2018-12-17 17:00:15 +0100

    Update spectroscopy.py
    v01

--- spectroscopy.py
+++ spectroscopy.py
@@ -1,5 +1,4 @@
 import numpy as np
-import shutil
 import datetime
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import style
@@ -9,57 +8,43 @@ import os
 style.use('seaborn-dark-palette')
 
 
-def copy_original(source, destination):
-
-    files = os.listdir(source)
-
-    for f in files:
-        shutil.copy(source + f, destination)
-
-
-def preprocessing(dire, n_line):
-
-    directory = os.fsencode(dire)
-
-    for file in os.listdir(directory):
-        filename = os.fsdecode(file)
-        print(filename)
-        path_to = dire + '/' + filename
-        n_firstlines = []
-
-        with open(path_to, 'r+') as f:
-            text = f.read()
-            f.seek(0)
-            f.truncate()
-            f.write(text.replace(',', '.'))
-
-        with open(path_to) as f, open("test.txt", "w") as out:
-            for x in range(n_line):
-                n_firstlines.append(next(f))
-            for line in f:
-                out.write(line)
-        os.remove(path_to)
-        os.rename('test.txt', path_to)
-
-
-def spectro_all(dire, a, b, graph_to):
+def spectro_all(original, a, b, graph_to, n_line):
 
     time = datetime.datetime.now()
-    directory = os.fsencode(dire)
+    directory = os.fsencode(original)
     list_of_powers = np.zeros((len(os.listdir(directory)), 2))
     num = 0
-    new_directory = graph_to + '/' + f'wyniki{time.strftime("%y%m%d_%H:%M")}'
-    new_directory_1 = graph_to + '/' + f'wykresy{time.strftime("%y%m%d_%H:%M")}'
-    os.mkdir(new_directory)
-    os.mkdir(new_directory_1)
+    m_time = f'/measurements{time.strftime("%y%m%d_%H:%M:%S")}'
+    g_time = f'/graphs{time.strftime("%y%m%d_%H:%M:%S")}'
+    new_directory_m = graph_to + m_time
+    new_directory_g = graph_to + g_time
+    os.mkdir(new_directory_m)
+    os.mkdir(new_directory_g)
 
     for file in os.listdir(directory):
+
+        file_name = os.fsdecode(file)
+        print(file_name)
+
+        with open(original + '/' + file_name, 'r+') as f:
+            text = f.read()
+            text1 = '\n'.join(text.split('\n')[n_line:])
+
+        with open(new_directory_m + '/' + file_name, 'w+') as f:
+            f.write(text1)
+
+        with open(new_directory_m + '/' + file_name, 'r+') as f:
+            t = f.read()
+            f.seek(0)
+            f.truncate()
+            f.write(t.replace(',', '.'))
+
+    for file in os.listdir(new_directory_m):
+
         filename = os.fsdecode(file)
+        data = np.loadtxt(new_directory_m + '/' + filename, delimiter='\t')
+        print(data.shape)
         print(filename)
-        path_to = dire + '/' + filename
-
-        data = np.loadtxt(path_to, delimiter="\t")
-
         y = []
         x = []
 
@@ -91,10 +76,10 @@ def spectro_all(dire, a, b, graph_to):
 
         result = gaussian(x, *popt)
 
+        filename = os.fsdecode(file)
         file_name = filename[:-6]
         power = float(file_name)
-        path_graph = new_directory_1 + '/' + f'{file_name}.png'
-        path_to_new = new_directory + '/' + f'{filename}'
+        path_graph = new_directory_g + f'/{file_name}.png'
 
         plt.plot(x, y, label=f'{file_name} mW')
         plt.plot(x, result, 'r--', label='fit')
@@ -105,12 +90,11 @@ def spectro_all(dire, a, b, graph_to):
         plt.clf()
 
         integrate = simps(result, dx=dx)
-        os.rename(path_to, path_to_new)
         list_of_powers[num] = power, integrate
         num += 1
 
-    path_powers = new_directory + '/' + f'list_of_powers.txt'
-    path_to_pg = new_directory_1 + '/' + f'intensity_to_power.png'
+    path_powers = new_directory_m + '/' + f'list_of_powers.txt'
+    path_to_pg = new_directory_g + '/' + f'intensity_to_power.png'
     lop = np.sort(list_of_powers.view('i8,i8'), order=['f1'], axis=0).view(np.float)
 
     x_p = np.array(lop[:, 0])
@@ -128,8 +112,3 @@ def spectro_all(dire, a, b, graph_to):
             f.write(f'{item}\n')
 
     return list_of_powers
-
-
-
-
-
