00afbf45d13b838c2034eed026ae00d205063d0e
sowizdrzal <45939141+sowizdrzal@users.noreply.github.com>
2018-12-17 17:03:55 +0100

    Update main.py
    v01

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import spectroscopy
 
-source = 'original/'
-dire = 'measurement'
+source = 'original'
 path_to_graph = 'graphs'
 a = 500
 b = 650
 line = 14
 
-spectroscopy.copy_original(source=source, destination=dire)
-spectroscopy.preprocessing(dire=dire, n_line=line)
-table = spectroscopy.spectro_all(dire=dire, a=a, b=b, graph_to=path_to_graph)
+
+spectro_all.spectro_all(original=source, a=a, b=b, graph_to=path_to_graph, n_line=line)
+
 
 
 
